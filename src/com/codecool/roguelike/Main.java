729417263d39b4a77d9b413e111a1eff97551a6c
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-04-20 11:35:34 +0200

    First commit

---
+++ src/com/codecool/roguelike/Main.java
@@ -0,0 +1,17 @@
+package com.codecool.roguelike;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JTextField textField = new JTextField();
+
+        textField.addKeyListener(new Game());
+        JFrame jframe = new JFrame();
+
+        jframe.add(textField);
+        jframe.setSize(100, 100);
+        jframe.setVisible(true);
+    }
+}
