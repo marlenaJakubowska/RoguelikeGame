0839bd50578f62001457722d7658b6e885922331
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-04-21 11:42:10 +0200

    Add mainMenu, move jframe to runGame

--- src/com/codecool/roguelike/Engine.java
+++ src/com/codecool/roguelike/Engine.java
@@ -2,5 +2,12 @@ package com.codecool.roguelike;
 
 public class Engine {
 
+    public static void clearScreen() {
+        try {
+            new ProcessBuilder("clear").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 
 }
--- src/com/codecool/roguelike/Ui.java
+++ src/com/codecool/roguelike/Ui.java
@@ -1,4 +1,13 @@
 package com.codecool.roguelike;
 
 public class Ui {
+
+    public static void printMainMenu() {
+        System.out.println("Welcome to Covid In The Forest Game. Please choose an option:");
+        System.out.println("1. New Game");
+        System.out.println("2. HighScores");
+        System.out.println("3. About");
+        System.out.println("0. Exit");
+    }
+
 }
