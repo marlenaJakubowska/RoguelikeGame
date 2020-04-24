8c543334ec9dc7e6b0964814862ae7738171fed6
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-04-24 09:14:41 +0200

    change tree string

--- src/com/codecool/roguelike/Engine.java
+++ src/com/codecool/roguelike/Engine.java
@@ -45,7 +45,7 @@ public class Engine {
         for(int i = 0; i < 100; i++) {
             int x = rand.nextInt(18) +1;
             int y = rand.nextInt(18) +1;
-            Obstacle tree = new Obstacle(new Coordinates(x, y), " ♈");
+            Obstacle tree = new Obstacle(new Coordinates(x, y), " T");
             board.addObjectToObstacles(tree);
             board.addObjectToTable(tree);
         }
